b73ab0ec47edbc374dd8a10c5974a21dbc20ebea
Kelvinorin <kelvinorin@gmail.com>
2021-05-21 14:11:38 +0800

    FRS

---
+++ contracts/FRS.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import "./owner/AdminRole.sol";
+
+
+contract FRS is ERC20Burnable, AdminRole {
+    using SafeMath for uint256;
+
+    constructor(string memory name_, string memory symbol_)
+        ERC20(name_, symbol_)
+    {
+        _mint(msg.sender, 1 * 10**18);
+    }
+
+    function mint(address recipient_, uint256 amount_)
+        public
+        onlyAdmin
+        returns (bool)
+    {
+        uint256 balanceBefore = balanceOf(recipient_);
+        _mint(recipient_, amount_);
+        uint256 balanceAfter = balanceOf(recipient_);
+
+        return balanceAfter > balanceBefore;
+    }
+
+    function burn(uint256 amount) public override onlyAdmin {
+        super.burn(amount);
+    }
+
+    function burnFrom(address account, uint256 amount)
+        public
+        override
+        onlyAdmin
+    {
+        super.burnFrom(account, amount);
+    }
+}
